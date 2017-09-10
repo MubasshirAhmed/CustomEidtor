99fbac2a280f9378ec53a386d01a7c5f57083f57
Mubasshir <Mubasshirahmed26@gmail.com>
2017-09-10 13:31:00 +0600

    Initial commit

---
+++ Assets/Sceripts/LevelScriptEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelScript))]
+public class LevelScriptEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        LevelScript myTarget = (LevelScript)target;
+        myTarget.experience = EditorGUILayout.IntField("Experience", myTarget.experience);
+        EditorGUILayout.LabelField("Level", myTarget.Level.ToString());
+
+        //myTarget.experience = EditorGUILayout.IntSlider("Val-you", myTarget.experience, 1, 10);
+        EditorGUILayout.IntSlider("Level", myTarget.Level, 1, 10);
+
+    }
+}
